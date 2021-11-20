d064afc3f52969b621ff6a61e089881c0af8515c
cml87 <gambarimas87@gmail.com>
2021-11-20 20:02:47 +0100

    specify ubuntu 20.04 tag in backup and restore scripts

--- backup_wiki.sh
+++ backup_wiki.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "creating file data.tar.gz with content of volume wikijs-db_vol ..."
-docker run --rm -v wikijs-db_vol:/data -v $(pwd):/backup ubuntu tar -zcf /backup/data.tar.gz /data
+docker run --rm -v wikijs-db_vol:/data -v $(pwd):/backup ubuntu:20.04 tar -zcf /backup/data.tar.gz /data
--- restore_wiki.sh
+++ restore_wiki.sh
@@ -4,4 +4,4 @@ echo "creating volume wikijs-db_vol. Nothing will be done if it already exists, 
 docker volume create wikijs-db_vol
 
 echo "extracting content of data.tar.gz into volume wikijs-db_vol"
-docker run --rm -v "$(pwd)"/data.tar.gz:/backup/data.tar.gz -v wikijs-db_vol:/data ubuntu tar -zxvf /backup/data.tar.gz -C /data --strip 1
+docker run --rm -v "$(pwd)"/data.tar.gz:/backup/data.tar.gz -v wikijs-db_vol:/data ubuntu:20.04 tar -zxvf /backup/data.tar.gz -C /data --strip 1
